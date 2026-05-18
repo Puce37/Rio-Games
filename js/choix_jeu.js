2b745c41aee048f14ada3a83c2b01636cb9e60ad
Puce37 <habertmathis3@gmail.com>
2026-05-18 13:47:34 +0200

    upgrade

--- js/choix_jeu.js
+++ js/choix_jeu.js
@@ -1,3 +1,19 @@
 function menu() {
   window.location.href = "../index.html";
 }
+
+function memo() {
+  window.location.href = "../jeu/memory/choix_version.html";
+}
+
+function puzzle() {
+  window.location.href = "../jeu/puzzle/pièces.html";
+}
+
+function labi() {
+  window.location.href = "..jeu/maze/maze.html";
+}
+
+function clic() {
+  window.location.href = "../jeu/clicker/clicker.html";
+}
