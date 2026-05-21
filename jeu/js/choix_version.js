6c67bddfd925113ca1303556b999e60af6cc9253
Puce37 <habertmathis3@gmail.com>
2026-05-21 12:15:28 +0200

    memory fix

---
+++ jeu/js/choix_version.js
@@ -0,0 +1,15 @@
+function M16() {
+  window.location.href = "./memory16.html";
+}
+
+function M32() {
+  window.location.href = "./memory32.html";
+}
+
+function M64() {
+  window.location.href = "./memory64.html";
+}
+
+function menu() {
+  window.location.href = "../choix_jeu.html";
+}
--- js/choix_version.js
+++
@@ -1,3 +0,0 @@
-function menu() {
-  window.location.href = "../choix_jeu.html";
-}
